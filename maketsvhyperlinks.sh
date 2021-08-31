9cce43fc1c34daff8355e11d732e32680cc642ff
Jim Procter <j.procter@dundee.ac.uk>
2021-08-31 13:27:41 +0100

    pipe from git-nomerged to transform its output to TSV with issue keys in eaach row of stdin as a hyperlink ready to paste into Google Sheets

--- maketsvhyperlinks.sh
+++ maketsvhyperlinks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# pipe from git-nomerged to transform its output to TSV with issue keys in eaach row of stdin as a hyperlink ready to paste into Google Sheets 
 perl -ne "print \"\n\";chomp; my \$l=\$_; my \$tb=(scalar \$l=~/\t/g); my @el=(\$l=~/(JAL-\d+)/g); print \$l; ((\$tb<4) and print \"\t\"x(3-\$tb)); local %k; foreach \$i (@el) { \$k{\$i}=1; }; @el = keys %k; foreach \$i (@el) { print \"\t=HYPERLINK(\\\"http://issues.jalview.org/browse/\$i\\\",\\\"\$i\\\")\"; };" 
