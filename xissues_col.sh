6427e69ed02be0d4db920c997c39663e9bfd6371
Jim Procter <j.procter@dundee.ac.uk>
2021-08-31 13:31:07 +0100

    yet another issue key extractor - reports unique list of issues found in stdin, one per line

--- xissues_col.sh
+++ xissues_col.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# yet another issue key extractor - reports unique list of issues found in stdin, one per line
 egrep  'JAL-\d+' | perl -ne 'my @el = ($_=~/(JAL-\d+)/mg); foreach $i (@el) { print "$i\n"; }' | sort -u 
