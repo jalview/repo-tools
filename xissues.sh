0e97dcd7ec081db14cb5ee662b29a0a7c9e4016a
Jim Procter <j.procter@dundee.ac.uk>
2021-08-31 13:29:30 +0100

    # another quick and dirty JAL- issue extractor reporting issues as CSV - slightly less perl and sorts the output

--- xissues.sh
+++ xissues.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# another quick and dirty JAL- issue extractor reporting issues as CSV - slightly less perl and sorts output
 egrep  'JAL-\d+' | perl -ne 'my @el = ($_=~/(JAL-\d+)/mg); foreach $i (@el) { print "$i\n"; }' | sort -u | perl -ne 'chomp; print $_.",";'
