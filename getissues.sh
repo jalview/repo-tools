ec45d8c701615b5f2cd227c57d376ef399c93098
Jim Procter <j.procter@dundee.ac.uk>
2021-08-31 13:18:53 +0100

    outputs JAL-.. issues in commit messages as CSV

--- getissues.sh
+++ getissues.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Usage - getissues.sh <git range specifier> - outputs JAL-.. issues in commit messages as CSV
 RANGE=$1
 
 git log --no-merges --oneline $RANGE | egrep  'JAL-\d+' | perl -ne 'my @ids = ($_=~/^(\S+) (.+)/); my @el = ($ids[1]=~/(JAL-\d+)/mg); foreach $i (@el) { print "$i\n"; }' | sort -u | perl -ne 'chomp; print $_.",";'
