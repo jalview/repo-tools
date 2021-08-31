a489442e6656e956bbc5ea730a2b09218f145e88
Jim Procter <j.procter@dundee.ac.uk>
2021-08-31 13:16:27 +0100

    wrap JAL-.. issue keys with an href link to issues.jalview.org

--- convtohtmllinks.sh
+++ convtohtmllinks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+# wrap JAL-.. issue keys with an href link to issues.jalview.org
 perl -npe 'my @el=($_=~/(JAL-\d+)/g); foreach $i (@el) { print "<a href=\"http://issues.jalview.org/browse/$i\">$i</a> "; }; print "/t";' 
